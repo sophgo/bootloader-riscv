b55b68a583cbd23bdff7e4f24e29699fe1e25b1d
Chao Wei <chao.wei@bitmain.com>
2023-04-14 21:36:32 +0800

    remove deprecated opensbi 1.0 and 0.8
    Change-Id: I0b47f966843645a815758466769f96380f9ca03d

--- opensbi/platform/sophgo/mango/gendtb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-input_list='mango.dts virt.dts'
-
-for input in $input_list; do
-    output=`basename $input .dts`.dtb
-    cpp -nostdinc -undef -x assembler-with-cpp $input | dtc -I dts -O dtb -o $output
-done
-
