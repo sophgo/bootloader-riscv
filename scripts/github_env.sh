64bbce0ce17159f5e41286b5acd141992b873a78
jingyu.li01 <jingyu.li01@sophgo.com>
2023-12-31 19:59:00 +0800

    scripts: Support the UEFI+GRUB2 boot flow
     - Refine the build_rv_grub ().
     - Add the EDKII and GRUB2 binaries into the Ubuntu and Fedora image to
       support the UEFI+GRUB2 boot flow on SG2042.
     - Update CI.
    
    Signed-off-by: jingyu.li01 <jingyu.li01@sophgo.com>

--- scripts/github_env.sh
+++ scripts/github_env.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 export RISCV64_LINUX_CROSS_COMPILE=riscv64-unknown-linux-gnu-
+export RISCV64_ELF_CROSS_COMPILE=riscv64-unknown-elf-
 export RV_ZSBL_SRC_DIR=$GITHUB_WORKSPACE/zsbl
 export RV_SBI_SRC_DIR=$GITHUB_WORKSPACE/opensbi
+export RV_EDKII_SRC_DIR=$GITHUB_WORKSPACE/sophgo-edk2
 export RV_KERNEL_SRC_DIR=$GITHUB_WORKSPACE/linux-riscv
 export PATH=${TOOLCHAIN_HOME}/bin:$PATH
